4cbf39059407a23003cdc8d7aa96142dcc687daf
Fialiis <fialiispro@gmail.com>
2025-06-05 17:35:45 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FialiisDemo() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Bienvenue sur FiaLiis</h1>
+      <p>Démo interactive de la plateforme d'entiercement.</p>
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FialiisDemo from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FialiisDemo />
+  </React.StrictMode>
+);
